5ddc5bb138b6f2c3b0af55eb7b357b2807d4d767
jobayer-alam-24 <158845902+jobayer-alam-24@users.noreply.github.com>
2024-11-12 10:43:12 +0600

    working

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace View_Component_Practice.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
