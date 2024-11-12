c5eff1e4f47a5a31263f5ab19a144d07a91d9eef
jobayer-alam-24 <158845902+jobayer-alam-24@users.noreply.github.com>
2024-11-12 09:28:01 +0600

    working

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace View_Component_Practice.Controllers
+{
+    public class ProductController : Controller
+    {
+    
+        [Route("Product/Products/{id?}")]
+        public IActionResult Products(int id)
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
